94b1a758affca780af0af37b6d1d05cd33692b98
moriyama1x4 <moriyama.1x4@gmail.com>
2018-07-13 01:30:45 +0900

    外部ファイル読み込み

---
+++ accounts.js
@@ -0,0 +1,3 @@
+var accounts = [['id1', 'password'], ['id2', 'password2']];
+
+module.exports = accounts;
---
+++ test.js
@@ -0,0 +1,8 @@
+//demo.js
+//configファイル読み込み
+var accounts = require('./accounts.js');
+
+console.log(accounts);
+console.log(accounts[0][1]);
+
+phantom.exit();
